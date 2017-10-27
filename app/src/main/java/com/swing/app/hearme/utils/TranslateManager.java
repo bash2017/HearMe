9eb4af45da33b7564658a4a3197148ac0d5b25e2
bash2017 <binary.dreams.factory@gmail.com>
2017-10-27 15:28:26 -0500

    Agrege la funcionabilidad total del traductor. Agrega imagenes extraidas  desde la bd.

--- app/src/main/java/com/swing/app/hearme/utils/TranslateManager.java
+++ app/src/main/java/com/swing/app/hearme/utils/TranslateManager.java
@@ -4,14 +4,16 @@ import android.content.Context;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.swing.app.hearme.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -33,44 +35,41 @@ public class TranslateManager {
     public void getTranslation(String text) {
         String[] words = text.split(" ");
         for (String word : words) {
-            ArrayList<ImageView> array = this.findWorld(word);
-            try {
-                if (array.size() > 0) {
-                    Log.i("swgA", "Entramos");
-                    for (int i = 0; i < array.size(); i++) {
-                        Log.i("swg", String.valueOf(i + "aaaa"));
-                        _llTranslatePics.addView(array.get(i));
+            Task dbTask = executeTaskFinder(word);
+            dbTask.addOnSuccessListener(new OnSuccessListener<ArrayList<ImageView>>() {
+                @Override
+                public void onSuccess(ArrayList<ImageView> arrayImages) {
+                    if (arrayImages != null) {
+                        for (int i = 0; i < arrayImages.size(); i++) {
+                            _llTranslatePics.addView(arrayImages.get(i));
+                        }
                     }
-                }else{
-                    Log.i("no","Entramos");
                 }
-            } catch (Exception e) {
-                Log.e("swg", e.getMessage());
-            }
-
+            });
         }
     }
 
-    private ArrayList<ImageView> findWorld(String world) {
-        ArrayList<ImageView> arrayImages = new ArrayList<>();
+
+    private Task executeTaskFinder(String world) {
+        final TaskCompletionSource<ArrayList<ImageView>> dbSource = new TaskCompletionSource<>();
+        Task dbTask = dbSource.getTask();
+
+        final ArrayList<ImageView> imagesArray = new ArrayList<>();
         DatabaseReference fireData = mDataBase.child(world);
         try {
             if (fireData != null) {
                 fireData.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        if (dataSnapshot != null && dataSnapshot.child("count").getValue() != null) {
-                            int images = dataSnapshot.child("count").getValue(Integer.class);
-                            Log.i("swg", String.valueOf(images));
-                            String url = String.valueOf(dataSnapshot.child("url1").getValue());
-                            Log.i("swg", url);
-                            /*for(int i=0; i < images; i++){
-                                String str = "url" + String.valueOf(i);
-                                String url = dataSnapshot.child(str).getValue().toString();
-                                Log.i("swg", url);
-                            }*/
-                        } else {
-                            Log.i("swg", "no, entramos1");
+                        if (dataSnapshot.exists()) {
+                            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(500, 600);
+                            for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                                ImageView newImage = new ImageView(_context);
+                                Picasso.with(_context).load(String.valueOf(postSnapshot.getValue())).into(newImage);
+                                newImage.setLayoutParams(layoutParams);
+                                imagesArray.add(newImage);
+                            }
+                            dbSource.setResult(imagesArray);
                         }
                     }
 
@@ -81,24 +80,8 @@ public class TranslateManager {
                 });
             }
         } catch (Exception e) {
-            Log.i("swg", e.getMessage());
+            Log.i("swg Error", e.getMessage());
         }
-        /*ArrayList<ImageView> images = new ArrayList<>();
-        if(word.equals("hola")){
-            ImageView newImage = new ImageView(_context);
-            newImage.setBackgroundResource(R.drawable.a);
-            images.add(newImage);
-        }*/
-        return arrayImages;
+        return dbTask;
     }
-
-    /*private ArrayList<ImageView> findWorld(String word){
-        ArrayList<ImageView> images = new ArrayList<>();
-        if(word.equals("hola")){
-            ImageView newImage = new ImageView(_context);
-            newImage.setBackgroundResource(R.drawable.a);
-            images.add(newImage);
-        }
-        return images;
-    }*/
 }
