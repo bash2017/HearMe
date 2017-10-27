2e5307b11ed7e930b29f4d2509433c7aa6932dd1
kuroisoft <31528890+kuroisoft@users.noreply.github.com>
2017-10-27 14:33:36 -0500

    Cambios para el reconocimiento de voz Activity de Samaniego

--- app/src/main/java/com/swing/app/hearme/TranslateActivity.java
+++ app/src/main/java/com/swing/app/hearme/TranslateActivity.java
@@ -1,5 +1,8 @@
 package com.swing.app.hearme;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +13,17 @@ import android.widget.Toast;
 
 import com.swing.app.hearme.utils.TranslateManager;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 
 public class TranslateActivity extends AppCompatActivity {
-
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
     private Button _btnTranslate;
     private EditText _txtTranslate;
     private LinearLayout _layTransImages;
     private TranslateManager _translateManager;
+    private Button _bntSpeechToText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +37,7 @@ public class TranslateActivity extends AppCompatActivity {
         _btnTranslate = (Button) findViewById(R.id.btnTranslateOk);
         _txtTranslate = (EditText) findViewById(R.id.txtTranslateText);
         _translateManager = new TranslateManager(getApplicationContext(),_layTransImages);
+        _bntSpeechToText = (Button)findViewById(R.id.btnSpeechToText);
     }
 
     private void setupEvents() {
@@ -45,6 +53,38 @@ public class TranslateActivity extends AppCompatActivity {
                 }
             }
         });
+        _bntSpeechToText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                        "Hola, ¿que quieres traducir?");
+                try {
+                    startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(),
+                            "No fue posible iniciar la caracteristica",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    _txtTranslate.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
     }
 
     private void clearTranslateLayout(){
